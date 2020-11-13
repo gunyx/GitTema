473a08e0e2cbfa4f1c5265d7d0f321cd23f3bf38
gunyx <gc.neacsu@yahoo.com>
2020-11-13 19:50:44 +0200

    Am modificat clasa magazin si clasa Electronice
    Am inclus o noua clasa "Specificatii", ti am modificat clasa Specificatii.
    Am modificat prin rebase.
    
    Signed-off-by: gunyx <gc.neacsu@yahoo.com>

---
+++ src/git/IngineriaProgramarii/Specificatii.java
@@ -0,0 +1,15 @@
+package git.IngineriaProgramarii;
+
+public class Specificatii {
+    int data;
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+
+    public int getData() {
+        return data;
+    }
+    
+}
