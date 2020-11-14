912c2f2c9365e464c9b95d99f7d14f31e891dc2e
laudan_daniel <daniel.laudan@mta.ro>
2020-11-14 23:14:08 +0200

    Implementare clasa Persoane si Program
    Au fost adaugare datele de stocare pentru clasele Persoane si Program, adaugate gettere si settere.
    
    Signed-off-by: laudan_daniel <daniel.laudan@mta.ro>

--- src/git/IngineriaProgramarii/Main.java
+++ src/git/IngineriaProgramarii/Main.java
@@ -2,7 +2,16 @@ package git.IngineriaProgramarii;
 
 public class Main {
     public static void main(String[] argv) {
+
         System.out.println("ceva");
+        Program program=new Program(10,11);
+        Personal daniel=new Persoane("daniel","daniel","angajat",22,program) {
+            @Override
+            void afisarePersoana() {
+                super.afisarePersoana();
+            }
+        };
+        daniel.afisarePersoana();
     }
 }
 
---
+++ src/git/IngineriaProgramarii/Personal.java
@@ -0,0 +1,12 @@
+package git.IngineriaProgramarii;
+
+abstract class Personal {
+    String nume;
+    String prenume;
+    int varsta;
+    String specializare;
+    Program program;
+
+    abstract void afisarePersoana();
+    abstract void adaugarePersoana(String nume, String prenume, String specializare, int varsta, Program program);
+}
