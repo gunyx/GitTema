10088bf1ab86859f3aa30ac63bb2513baf8f9300
laudan_daniel <daniel.laudan@mta.ro>
2020-11-14 23:52:25 +0200

    Modificare clasa Program
    Adaugare functionalitati de a afisa programul sau de al schimba
    
    Signed-off-by: laudan_daniel <daniel.laudan@mta.ro>

--- src/git/IngineriaProgramarii/Program.java
+++ src/git/IngineriaProgramarii/Program.java
@@ -11,6 +11,15 @@ public class Program {
         this.ore_lucru=ora_final-ora_inceput;
     }
 
+    public void afisare(){
+        System.out.println("Programul incepe la:"+this.ora_inceput+" si se termina la:"+this.ora_final+". Total de:"+this.ore_lucru+" ore");
+    }
+
+    public void schimba(int ora_inceput, int ora_final){
+        this.ora_final=ora_final;
+        this.ora_inceput=ora_inceput;
+        this.ore_lucru=this.ora_final-this.ora_inceput;
+    }
     public void setOre_lucru(int ore_lucru) {
         this.ore_lucru = ore_lucru;
     }
